e1f98a25c9db1fa4ff3a2ee9d5351624d96632f1
Anna Sokolova <sokolovaskorik@gmail.com>
2020-10-01 01:15:28 +0300

    Примеры итерации массива с циклом for

---
+++ js/arrive.js
@@ -0,0 +1,44 @@
+//  ***********Итерация по массиву
+// Переберем массив клиентов и выведем их имена.
+
+const clients = [
+  'Mango',
+  'Ajax',
+  'Poly',
+  'Sona',
+  'Nika',
+  'Kat',
+  'Vika',
+  'Pola',
+];
+
+for (let i = 0; i < clients.length; i += 1) {
+  console.log(`Logging clients: ${clients[i]} - ${i + 1}`);
+}
+
+// Выыедет в консоль список клиентов с нумерацией по порядку
+
+// Logging clients: Mango - 1
+// Logging clients: Ajax - 2
+// Logging clients: Poly - 3
+// Logging clients: Sona - 4
+// Logging clients: Nika - 5
+// Logging clients: Kat - 6
+// Logging clients: Vika - 7
+// Logging clients: Pola - 8
+
+// ${clients[i]} или ${значение элемента} - это имя переменной в массиве - Mango, Ajax....
+// ${i + 1} или ${номер элемента} это счетчик(нумерация по списку с еденицы-1) - 1, - 2....
+
+// ************    С помощью цикла массив можно заполнить данными.
+
+const numbers = [];
+
+for (let i = 0; i < 5; i += 1) {
+  numbers.push(`label-${i}`); // добавляет новые элементы в массив
+}
+
+console.log('numbers: ', numbers);
+
+// Выведет в консоль новый массив
+// numbers:  (5) ["label-0", "label-1", "label-2", "label-3", "label-4"]
